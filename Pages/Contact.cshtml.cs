cc85a39a231fe8e356fcdfc331b45d488d7371b0
Hamid47hrs <hamid47.hrs@gmail.com>
2024-02-11 00:05:23 +0330

    Make the code simpler and post form data to SQL database.

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MyApp.Namespace
 {
@@ -8,43 +10,102 @@ namespace MyApp.Namespace
     {
         public void OnGet() { }
 
+        [BindProperty]
+        [Required(ErrorMessage = "The First Name is required.")]
+        [Display(Name = "First Name *")]
         public string FirstName { get; set; } = "";
 
+        [BindProperty, Required(ErrorMessage = "The Last Name is required.")]
+        [Display(Name = "Last Name *")]
         public string LastName { get; set; } = "";
 
+        [BindProperty, Required(ErrorMessage = "The Email is required.")]
+        [Display(Name = "E-mail *")]
+        [EmailAddress]
         public string Email { get; set; } = "";
 
+        [BindProperty]
         public string? Phone { get; set; } = "";
 
+        [BindProperty, Required]
+        [Display(Name = "Subject *")]
         public string Subject { get; set; } = "";
 
+        [BindProperty, Required(ErrorMessage = "The Message is required.")]
+        [MinLength(5, ErrorMessage = "The Message should be at least 5 characters.")]
+        [MaxLength(1024, ErrorMessage = "The Message should be at Most 1024 characters.")]
+        [Display(Name = "Message *")]
         public string Message { get; set; } = "";
 
+        public List<SelectListItem> SubjectList { get; } =
+            new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Value = "Order Status",
+                    Text = "Order Status",
+                    Selected = true
+                },
+                new SelectListItem { Value = "Refund Request", Text = "Refund Request" },
+                new SelectListItem { Value = "Job Application", Text = "Job Application" },
+                new SelectListItem { Value = "Other", Text = "Other" },
+            };
         public string SuccessMessage { get; set; } = "";
         public string ErrorMessage { get; set; } = "";
 
         public void OnPost()
         {
-            FirstName = Request.Form["firstname"];
-            LastName = Request.Form["lastname"];
-            Email = Request.Form["email"];
-            Phone = Request.Form["phone"];
-            Subject = Request.Form["subject"];
-            Message = Request.Form["message"];
-
-            if (
-                FirstName.Length == 0
-                || LastName.Length == 0
-                || Email.Length == 0
-                || Subject.Length == 0
-                || Message.Length == 0
-            )
+            if (!ModelState.IsValid)
             {
                 ErrorMessage = "Please fill all required fields.";
                 return;
             }
 
+            if (Phone == null)
+                Phone = "";
+
+            try
+            {
+                string connectionString =
+                    "Data Source=localhost;Initial Catalog=master;Integrated Security=SSPI;User ID=myDomain\\sa;Password=tlou2;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql =
+                        "INSERT INTO messages"
+                        + "(first_name, last_name, email, phone, subject, message) VALUES "
+                        + "(@firstname, @lastname, @email, @phone, @subject, @message);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@firstname", FirstName);
+                        command.Parameters.AddWithValue("@lastname", LastName);
+                        command.Parameters.AddWithValue("@email", Email);
+                        command.Parameters.AddWithValue("@phone", Phone);
+                        command.Parameters.AddWithValue("@subject", Subject);
+                        command.Parameters.AddWithValue("@message", Message);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return;
+            }
+
             SuccessMessage = "Your Form is submitted successfully.";
+
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Phone = "";
+            Subject = "";
+            Message = "";
+
+            ModelState.Clear();
         }
     }
 }
